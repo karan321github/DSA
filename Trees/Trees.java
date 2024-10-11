cd75312e2943ff36a50724b0daa0060c75e3bf73
KARAN SINGH <131857266+karan321github@users.noreply.github.com>
2024-10-12 00:02:04 +0530

    Update Trees.java

--- Trees/Trees.java
+++ Trees/Trees.java
@@ -390,11 +390,11 @@ class HelloWorld {
                 System.out.println();
                 tree.KthLevelIterative(root, 3);
                 System.out.println(tree.lca(root, 4, 5).data);
-<<<<<<< HEAD
+
                 System.out.println(tree.minDistanceBetweenTwoNodes(root, 4, 5));
                 System.out.println(tree.KthAnccestor(root , 5 , 2));
-=======
+
                 System.out.println(tree.minDistanceBetweenTwoNodes(root, 4, 2));
->>>>>>> 6389785a39313a4f7574219c2d393448b4d2f5eb
+
         }
 }
