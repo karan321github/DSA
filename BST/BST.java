db312f3557c3b25e1bc4e6abf2b86368fb645924
Karan Singh <131857266+karan321github@users.noreply.github.com>
2025-01-04 23:49:41 +0530

    Added root to leaf path

--- BST/BST.java
+++ BST/BST.java
@@ -1,5 +1,7 @@
 package BST;
 
+import java.util.ArrayList;
+
 public class BST {
         class Node {
                 int data;
@@ -90,6 +92,59 @@ public class BST {
                 return root;
         }
 
+        public static void printInRange(Node root, int k1, int k2) {
+                if (root == null) {
+                        return;
+                }
+                if (root.data <= k2 && root.data >= k1) {
+                        printInRange(root.left, k1, k2);
+                        System.out.print(root.data + " ");
+                        printInRange(root.right, k1, k2);
+                } else if (root.data <= k2) {
+                        printInRange(root.right, k1, k2);
+                } else {
+
+                        printInRange(root.left, k1, k2);
+                }
+        }
+
+        private static void printPath(ArrayList<Integer> path) {
+                for (int i = 0; i < path.size(); i++) {
+                        {
+                                System.out.print(path.get(i) + "-");
+                        }
+                }
+        }
+
+        public static void printRootToLeafPath(Node root, ArrayList<Integer> path) {
+                if (root == null) {
+                        return;
+                }
+                path.add(root.data);
+                if (root.left == null && root.right == null) {
+                        printPath(path);
+                        System.out.println();
+                }
+                printRootToLeafPath(root.left, path);
+                printRootToLeafPath(root.right, path);
+                path.remove(path.size() - 1);
+        }
+
+        public static void validateBst(Node root) {
+                if (root == null) {
+                        return;
+                }
+                ArrayList<Integer> list = new ArrayList<>();
+                validateBst(root.left);
+                list.add(root.data);
+                System.out.print(root.data + " ");
+                validateBst(root.right);
+
+                // for (int i = 0; i < list.size(); i++) {
+                //         System.out.print(list.get(i) + " ");
+                // }
+        }
+
         public static void main(String[] args) {
                 BST newBST = new BST();
                 int nodes[] = { 8, 5, 3, 1, 4, 6, 10, 11, 14 };
@@ -98,12 +153,17 @@ public class BST {
                 for (int i = 0; i < nodes.length; i++) {
                         root = newBST.insert(root, nodes[i]);
                 }
+
                 // newBST.inorder(root);
                 // System.out.println();
                 // System.out.println(newBST.searchInBST(root, 7));
                 // System.out.println();
-                root = deleteNode(root, 10);
-                System.out.println();
-                inorder(root);
+                // root = deleteNode(root, 10);
+                // System.out.println();
+                // inorder(root);
+                // printInRange(root, 5, 10);
+                // System.out.println();
+                // printRootToLeafPath(root, new ArrayList<>());
+                validateBst(root);
         }
 }
